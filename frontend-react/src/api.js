1750a3fe26122774b3d5835f964f1b0a39abce1f
Hamid Nadeem <hamidnadeemn043@gmail.com>
2026-06-19 00:29:31 +0500

    changes for deployment

--- frontend-react/src/api.js
+++ frontend-react/src/api.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://localhost:8080/api' });
+const api = axios.create({ 
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080/api' 
+});
+
 
 api.interceptors.request.use(cfg => {
   const token = localStorage.getItem('inkwell_token');
